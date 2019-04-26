d34cf1f6586c78529c2389b2df9bc4c75fb2374e
Jakub Półgęsek <jakub@polgesek.pl>
2019-04-27 01:13:51 +0200

    Source HTML doesn't have to be named index.html anymore

--- deployconfig.py
+++ deployconfig.py
@@ -28,6 +28,7 @@ class DeployConfig:
 		self.output_dir = self._gen_path(data["output_dir"])
 		self.source_css = self._gen_path(data["source_css"])
 		self.source_html = self._gen_path(data["source_html"])
+		self.raw_source_html = data["source_html"]
 		self.static_dirs = data["static_dirs"]
 		self.static_files = data["static_files"]
 		self.hash_this = data["hash_this"]
